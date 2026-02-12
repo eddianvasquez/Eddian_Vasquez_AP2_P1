cf4fb66e5724c95502f2eb2b06e0077503649fce
eddianvasquez <eddian_vasquez@ucne.edu.do>
2026-02-12 18:21:57 -0400

    moviendo y eliminando archivos inecesarios

--- app/src/main/java/edu/ucne/eddian_vasquez_ap2_p1/domain/Cerveza.kt
+++
@@ -1,8 +0,0 @@
-package edu.ucne.Eddian_Vasquez_Ap2_p1.domain.model
-
-data class Cerveza(
-    val idCerveza: Int? = null,
-    val nombre: String,
-    val marca: String,
-    val puntuacion: Int
-)
