781004720fc88f9d476ccc9258212a0cd0c067ab
eddianvasquez <eddian_vasquez@ucne.edu.do>
2026-02-12 18:28:44 -0400

    arreglando componentes de navegacion

--- app/src/main/java/edu/ucne/eddian_vasquez_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/eddian_vasquez_ap2_p1/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.Eddian_Vasquez_Ap2_p1.presentation.navigation.BeerTrackerNavHost
+import edu.ucne.eddian_vasquez_ap2_p1.presentation.navigation.BeerTrackerNavHost
 import edu.ucne.eddian_vasquez_ap2_p1.ui.theme.Eddian_Vasquez_AP2_P1Theme
 
 @AndroidEntryPoint
